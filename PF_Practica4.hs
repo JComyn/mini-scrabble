5e3ed1c76464e220638f5e53262953b32a2809ff
Marco <marco.prieto@alumnos.upm.es>
2024-05-18 14:50:59 +0200

    Cambinaciones no deuelve vacio

--- PF_Practica4.hs
+++ PF_Practica4.hs
@@ -4,3 +4,6 @@ import Data.List (subsequences, permutations)
 -- Genera todas las permutaciones de una cadena de caracteres.
 allPermutations :: String -> [String]
 allPermutations s = concatMap permutations (subsequences s)
+
+combinaciones :: String -> [String]
+combinaciones = concatMap (filter (not . null) . permutations) . subsequences
